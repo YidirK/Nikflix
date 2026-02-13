c3b0683a3288d2817b61ef3c7d2d2673dc1c48bd
variableninja <94451745+variableninja@users.noreply.github.com>
2026-02-13 11:57:00 -0100

    feat: implement dynamic script loading based on controller type

--- chromium/netflix-interceptor-loader.js
+++ chromium/netflix-interceptor-loader.js
@@ -1,7 +1,7 @@
-browser.storage.local.get("controllerType").then(function(data) {
+chrome.storage.local.get("controllerType", function(data) {
   if (data.controllerType === "netflix") {
     var s = document.createElement("script");
-    s.src = browser.runtime.getURL("netflix-interceptor.js");
+    s.src = chrome.runtime.getURL("netflix-interceptor.js");
     (document.head || document.documentElement).appendChild(s);
   }
 });
